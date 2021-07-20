5000523821415680e25db25e8b9a945c3b34106b
Muaz Yhaya <muazyhaya@outlook.com>
2021-07-19 19:51:57 -0700

    commit tree 8d9a6c9ee423afcb7e65b8a3c509494e15d86ace

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var num1 = 31
+var num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+var num3 = 16
+var num4 = 6
+const mod = num3 %= num4;
+const max = Math.max(1,5,10,15,20);
+
